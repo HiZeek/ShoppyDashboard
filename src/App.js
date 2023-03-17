88a5f1fe1c8f519f615cc5b734d9fb369ccda6e2
Isaac Idibia <idibiaisaac3@gmail.com>
2023-03-17 06:23:10 +0100

    initial setup, sidebar, navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { FiSettings } from "react-icons/fi";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
+
+import Sidebar from "./components/layout/Sidebar";
+import Navbar from "./components/layout/Navbar";
+import Ecommerce from "./pages/Ecommerce";
+import Orders from "./pages/Orders";
+import Employees from "./pages/Employees";
+import Customer from "./pages/Customer";
+import Kanban from "./pages/Kanban";
+import Editor from "./pages/Editor";
+import Calendar from "./pages/Calendar";
+import ColorPicker from "./pages/ColorPicker";
+import Line from "./pages/Charts/Line";
+import Area from "./pages/Charts/Area";
+import Bar from "./pages/Charts/Bar";
+import Pie from "./pages/Charts/Pie";
+import Financial from "./pages/Charts/Financial";
+import ColorMapping from "./pages/Charts/ColorMapping";
+import Pyramid from "./pages/Charts/Pyramid";
+import Stacked from "./pages/Charts/Stacked";
+import { useStateContext } from "./context/ContextProvider";
 
 function App() {
+  const { activeMenu } = useStateContext();
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* dashboard */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              {/* pages */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customer />} />
+
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
